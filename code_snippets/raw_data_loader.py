cd8a4e6fd58af8347eb8973ca5f83ae3f25a3804
Jared J Brannan <jared.brannan@wsu.edu>
2021-09-10 03:34:58 +0000

    finish raw loaders

--- code_snippets/raw_data_loader.py
+++ code_snippets/raw_data_loader.py
@@ -1,13 +1,25 @@
-import pandas
+"""
+Description: load in the files from raw_data. Creates the following variables as pandas dataframes:
+    - boundary_raw_df
+    - airlines_raw_df
+    - airports_raw_df
+    - routes_raw_df
+"""
+import pandas as pd
 
 def load_boundary_data():
-    pass
+    return pd.read_csv("raw_data/boundary-each-state.tsv", sep="\t", header=None)
 
 def load_airlines_data():
-    pass
+    return pd.read_csv("raw_data/airlines.dat", header=None)
 
 def load_airports_data():
-    pass
+    return pd.read_csv("raw_data/airports.dat", header=None)
 
 def load_routes_data():
-    pass
+    return pd.read_csv("raw_data/routes.dat", header=None)
+
+boundary_raw_df = load_boundary_data()
+airlines_raw_df = load_airlines_data()
+airports_raw_df = load_airports_data()
+routes_raw_df   = load_routes_data()
--- tests/test_raw_data_loader.py
+++ tests/test_raw_data_loader.py
@@ -1,28 +1,24 @@
 from code_snippets.raw_data_loader import *
 
 def test_boundary_data():
-    assert 'boundary_raw_df' in locals()
     # test row number
     assert len(boundary_raw_df.index) == 50
     # test column number
-    assert len(boundary_raw_df.columns) == 3
+    assert len(boundary_raw_df.columns) == 2
 
 def test_airlines_data():
-    assert 'airlines_raw_df' in locals()
     # test row number
     assert len(airlines_raw_df.index) == 6162
     # test column number
     assert len(airlines_raw_df.columns) == 8
 
 def test_airports_data():
-    assert 'airports_raw_df' in locals()
     # test row number
     assert len(airports_raw_df.index) == 7698
     # test column number
     assert len(airports_raw_df.columns) == 14
 
 def test_routes_data():
-    assert 'routes_raw_df' in locals()
     # test row number
     assert len(routes_raw_df.index) == 67663
     # test column number
